4b882319622e61c3ff8fd8c475e45ae23343c944
Philip Crotwell <crotwell@seis.sc.edu>
2018-07-27 08:42:13 -0400

    swtich to operatorCode

--- app/routes/equipment-installations/index.js
+++ app/routes/equipment-installations/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.query('equipmentInstallation', { primaryoperatorcode_in: 'SCSN-SC' });
+    return this.store.query('equipmentInstallation', { operatorcode_in: 'SCSN-SC' });
   }
 });
