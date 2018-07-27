a95393a8d2f1087055cf0e685c978609e7ec4427
Philip Crotwell <crotwell@seis.sc.edu>
2018-07-27 08:50:19 -0400

    remove _in from query param, link to site operators

--- app/routes/telemetry-connections/index.js
+++ app/routes/telemetry-connections/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-      return this.store.query('telemetryConnection', { operatorcode_in: 'SCSN-SC' });
+      return this.store.query('telemetryConnection', { operatorcode: 'SCSN-SC' });
     }
 });
